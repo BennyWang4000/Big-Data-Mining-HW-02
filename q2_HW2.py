bd0f6d54c4a53ba32e571f20e20ed8a8e15531ec
BennyWang4000 <wang.benny0102@gmail.com>
2023-11-18 16:48:38 +0800

    231118 q4 done

---
+++ q2_HW2.py
@@ -0,0 +1,44 @@
+from time import time
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+import os
+
+
+def count(row):
+    lst = []
+    for c in row:
+        lst.append((c, 1))
+    return lst
+
+
+def parse_by_hour(row, period):
+    lst = []
+    tot = 0
+    for i in range(2, 145, period):
+        for j in range(i, i + period):
+            if j < 145:
+                tot += int(row.asDict()['TS' + str(i)])
+        lst.append(tot)
+        tot = 0
+    return lst
+
+
+period_dct = {3: 'hour', 72: 'day'}
+if __name__ == "__main__":
+    conf = SparkConf()
+    conf.setMaster(
+        'spark://DESKTOP-I95HRMP.localdomain:12222').setAppName("Q2")
+    sc = SparkContext(conf=conf)
+    spark = SparkSession(sc)
+
+    # hour
+    for file_path in ['./data/platform/Facebook.csv', './data/platform/GooglePlus.csv', './data/platform/LinkedIn.csv']:
+        platform = file_path.split('/')[-1].replace('.csv', '')
+        df = spark.read.csv(file_path, sep=',', header=True).rdd
+
+        for period in [3, 72]:
+            rdd_avg = df.flatMap(lambda x: parse_by_hour(x, period))
+            rdd_avg = rdd_avg.mean()
+            with open(os.path.join('runs', 'q2_' + platform + '_' + period_dct[period] + '_avg.txt'), '+a') as file:
+                file.write(str(rdd_avg))
---
+++ q4_HW2.py
@@ -0,0 +1,65 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from nltk.tokenize import word_tokenize
+import numpy as np
+import pandas as pd
+import os
+
+
+def parse_words(row, col):
+    tokens = word_tokenize(str(row.asDict()[col]))
+    res = []
+    for token in tokens:
+        res.append((token, 1))
+    return res
+
+
+def parse_cooccurrence(row, col, words):
+    res = []
+    for i, (word_a, _) in enumerate(words):
+        for word_b, _ in words[i:]:
+            try:
+                if word_a in row.asDict()[col] and word_b in row.asDict()[col]:
+                    res.append(((word_a, word_b), 1))
+            except Exception as e:
+                print('NoneType')
+    return res
+
+
+if __name__ == '__main__':
+
+    DATA_PATH = './data/News_Final_clean.csv'
+    OUTPUT_DIR = './runs'
+
+    conf = SparkConf()
+    conf.setMaster(
+        'spark://localhost:12222').setAppName("Q4")
+    conf.setAppName("Q1")
+    sc = SparkContext(conf=conf)
+    spark = SparkSession(sc)
+
+    rows = spark.read.csv(DATA_PATH, sep=',', header=True).rdd
+
+    for col in ['Title', 'Headline']:
+        words = rows.flatMap(lambda row: parse_words(row, col))\
+                    .reduceByKey(lambda times1, times2: times1 + times2)\
+                    .takeOrdered(100, key=lambda times: -times[1])
+        for topic in ['economy', 'microsoft', 'obama', 'palestine']:
+            coocu_mat = [[]] * 101
+            coocu = rows.filter(lambda row: row.asDict()['Topic'] == topic)\
+                        .flatMap(lambda row: parse_cooccurrence(row, col, words))\
+                        .reduceByKey(lambda times1, times2: times1 + times2)\
+                        .sortBy(lambda pair: pair[0][0])\
+                        .collect()
+            tags = []
+            for (t1, t2), _ in coocu:
+                tags += [t1, t2]
+            tags = index = columns = sorted(list(set(tags)))
+            tags = dict((t, i) for i, t in enumerate(tags))
+            coocu_mat = np.identity(len(tags))
+            for (t1, t2), co in coocu:
+                coocu_mat[tags[t1]][tags[t2]] = co
+                coocu_mat[tags[t2]][tags[t1]] = co
+            df = pd.DataFrame(coocu_mat, index=index, columns=columns)
+            df.to_csv(OUTPUT_DIR + '/q4_' + col + '_' + topic + '.csv')
